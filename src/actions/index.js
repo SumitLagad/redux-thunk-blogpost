b43a1f7e1ae3ca1ff401087d1c6523625b0a7816
SumitLagad <sumitlagad7@gmail.com>
2019-08-27 10:22:01 +0530

    displyed list of blogs from reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,8 @@ import jsonPlaceholder from '../api/jsonPlaceholder'
 
 export const fetchPosts = () => async (dispatch) => {
         const responce = await jsonPlaceholder.get('/posts')
-    
         dispatch({
-            type: 'FETCH_POST',payload : responce
+            type: 'FETCH_POSTS', payload : responce.data
         })
-    };
+};
 
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,16 +5,37 @@ import { fetchPosts } from '../actions';
 class BlogList extends React.Component {
 
     componentDidMount() {
-        console.log(this.props.fetchPosts());
+        this.props.fetchPosts();
+    }
+
+    renderList () {
+        return this.props.posts.map( post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />                        
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+
+                    </div>
+                </div>
+            );
+        });
     }
 
     render() {
         return(
-            <div>
-                Blog List
+            <div className="ui relaxed divided list"> 
+                {this.renderList()}
             </div>
         );
     }
 }
 
-export default connect(null,{fetchPosts})(BlogList);
+const mapStateToProps = (state) => {
+    return { posts: state.posts};
+}
+
+export default connect(mapStateToProps,{fetchPosts})(BlogList);
